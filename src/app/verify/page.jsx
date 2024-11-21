f5d537d372ed5331cdc5d464261289bf77c1fcc9
ashraful malik <ashrafulmalik71@gmail.com>
2024-11-21 12:03:04 +0530

    Update verification page to improve user authentication flow

--- src/app/verify/page.jsx
+++ src/app/verify/page.jsx
@@ -1,6 +1,6 @@
 "use client";
-import { useRouter, useSearchParams, Suspense } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Suspense, useEffect, useState } from "react";
 import { account } from "../index";
 import SpinLoader from "../../components/ui/SpinLoader";
 const VerifyEmail = () => {
